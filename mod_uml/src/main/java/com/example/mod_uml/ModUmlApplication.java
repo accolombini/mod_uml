a7ec0796af0731c64fc17ebc6c9b6d892598469f
Angelo <accolombini@gmail.com>
2021-11-16 16:47:01 -0300

    Projeto criado

---
+++ mod_uml/src/main/java/com/example/mod_uml/ModUmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mod_uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModUmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModUmlApplication.class, args);
+    }
+
+}
